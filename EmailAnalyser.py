72ada97b02e6dd9bba49c6e1d170d745a1e52d85
TryggviThor <tryggvithorg@gmail.com>
2015-03-25 20:19:07 +0000

    First iteration of mostCommonWordsByMonth

--- EmailAnalyser.py
+++ EmailAnalyser.py
@@ -10,19 +10,21 @@ def main():
     print 'Building tree...'
     tree = ET.parse(fileName)
     print ' > Tree built.'
+    print ''
 
     root = tree.getroot()
 
-    totalEmails(root)
-    reAndFwdCounts(root)
-    countByMonth(root)
-    countBySender(root)
-    mostCommonWords(root)
+    # totalEmails(root)
+    # reAndFwdCounts(root)
+    # countByMonth(root)
+    # countBySender(root)
+    # mostCommonWords(root)
+    mostCommonWordsByMonth(root)
 
 def totalEmails(root):
     print 'Counting...'
     counter = 0
-    for child in root.iter('subject'):
+    for child in root.iter('message'):
         counter+=1
     print 'Total emails:',counter 
 
@@ -66,13 +68,18 @@ def countByMonth(root):
               '11': 'November',
               '12': 'December' }
 
+    newyear = ''
     for key in sorted(dates):
         # Seperating year and month
         yyyy = str(key)[:4]
         mm = str(key)[4:6]
+        if yyyy != newyear:
+            print '\n'+yyyy+':'
+        newyear = yyyy
         month = months[mm]
                 
-        print month,yyyy+':\t\t'+str(dates[key])
+        print '\t'+month+':\t\t'+str(dates[key])
+    print ''
 
 def countBySender(root):
     print 'Counting emails by sender...'
@@ -93,6 +100,7 @@ def countBySender(root):
 
 def mostCommonWords(root):
     print 'Fifteen most common words:'
+
     words = {}
     for child in root.iter('message'):
         text = child.find('text').text.split()
@@ -110,9 +118,67 @@ def mostCommonWords(root):
             break
         print str(key)+':\t'+str(words[key])
         i += 1
+    print ''
 
 def mostCommonWordsByMonth(root):
-    pass
+    print 'Fifteen most common words by month:'
+    
+    dates = {}
+    for child in root.iter('message'):
+        yyyymm = child.find('received').find('date').text[:6]
+
+        # Count the words for the current month
+        date = {}
+        text = child.find('text').text.split()
+
+        for word in text:
+            word = word.encode('utf-8', 'ignore')
+            if word not in date:
+                date[word] = 1
+            else:
+                date[word] += 1
+
+        # Add the wordcount of the current month to the total
+        if yyyymm not in dates:
+            dates[yyyymm] = date
+        else:
+            dates[yyyymm].update(date)
+
+
+    # Print the results in a readable manner
+    months = {'01': 'January',
+              '02': 'February',
+              '03': 'March',
+              '04': 'April',
+              '05': 'May',
+              '06': 'June',
+              '07': 'July',
+              '08': 'August',
+              '09': 'September',
+              '10': 'October',
+              '11': 'November',
+              '12': 'December' }
+
+    for date in sorted(dates):
+        i = 0
+
+        yyyy = str(date)[:4]
+        mm = str(date)[4:6]
+        month = months[mm]
+
+        print month,yyyy+':'
+
+        for key in sorted(dates[date], key=dates[date].get, reverse=True):
+            if i > 10:
+                break
+
+            # Separating year and month
+            
+                    
+            print '\t'+key+':\t\t'+str(dates[date][key])
+            i += 1
+    print ''
+
 
 def mostCommonWordsBySender(root):
     pass
